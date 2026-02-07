093d821ff757fe2547d7e7398acb26d433781c76
Jay Coskey <jay.coskey@gmail.com>
2026-02-07 13:25:57 -0800

    Change filename from board_condition_flags.py to board_state.py, and name of class within. Role for this class: store the determinations of check, checkmate, and stalemate made after every move.

---
+++ src/board_state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# by Jay M. Coskey, 2026
+
+from enum import auto, Enum
+
+
+class BoardState(Enum):
+    Normal = auto()
+    Check = auto()
+    Checkmate = auto()
+    Stalemate = auto()
