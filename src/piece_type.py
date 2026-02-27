877275536ebca36ec5c3a53ffc4c80af48f7a778
Jay Coskey <jay.coskey@gmail.com>
2026-02-26 16:12:46 -0800

    Add to_unicode() method to PieceType, to support unicode symbols in the board's print() method

--- src/piece_type.py
+++ src/piece_type.py
@@ -65,3 +65,24 @@ class PieceType(Enum):
             raise NotImplementedError(f'Piece Type characters not supported for language {lang}')
 
         return PT_TO_SYMBOL[self.name]
+
+    def to_unicode(self, player):
+        unicode_symbols = [
+                {
+                    'King':   '\u2654',
+                    'Queen':  '\u2655',
+                    'Rook':   '\u2656',
+                    'Bishop': '\u2657',
+                    'Knight': '\u2658',
+                    'Pawn':   '\u2659'
+                },
+                {
+                    'King':   '\u265A',
+                    'Queen':  '\u265B',
+                    'Rook':   '\u265C',
+                    'Bishop': '\u265D',
+                    'Knight': '\u265E',
+                    'Pawn':   '\u265F'
+                }]
+        return unicode_symbols[player][self.name]
+
