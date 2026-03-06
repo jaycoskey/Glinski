8923624a40b926db621d77610a947ac6fcf04f6f
Jay Coskey <jay.coskey@gmail.com>
2026-03-05 18:37:49 -0800

    Remove HexPos rank() method that duplicates Geometry.npos_to_rank()

--- src/hex_pos.py
+++ src/hex_pos.py
@@ -41,7 +41,3 @@ class HexPos:
     def __sub__(self, other: "HexPos"):
         return HexVec(self.hex0 - other.hex0, self.hex1 - other.hex1)
 
-    # GREP: Choice of coordinates
-    # TODO: DESIGN: Consider moving this to geometry.py.
-    def rank(self):
-        return 6 - (self.hex0 if self.hex0 > 0 else 0) + self.hex1
